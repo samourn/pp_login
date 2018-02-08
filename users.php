b9061a50b2d5e483804785f261d98180aa40b685
Sam Ourn <sourn@mail.greenriver.edu>
2018-02-08 13:14:26 -0500

    Created User class and constructor

---
+++ users.php
@@ -0,0 +1,22 @@
+<?php
+
+class User
+{
+    //Declare the class attributes
+    private $username;
+    private $password;
+    private $loggedIn;
+
+    //Parameterized constructor
+    public function __construct()
+    {
+        $this->username = "";
+        $this->loggedIn = false;
+    }
+
+    //getters and setters
+
+
+}//end User class
+
+?>
